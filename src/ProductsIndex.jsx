1ffd9dae60a84469028289991e083b362f368920
bradzara <bradzara93@gmail.com>
2024-07-02 12:12:19 -0700

    Make search function case sensitive

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -7,21 +7,30 @@ export function ProductsIndex(props) {
   return (
     <div id="products-index">
       <h1>All products</h1>
-      <p>Search: <input type="text" value={searchTerm} onChange={event => setSeachTerm(event.target.value)} placeholder="Search for items..." list="titles" /></p>
+      <p>Search: 
+        <input 
+        type="text" 
+        value={searchTerm} 
+        onChange={event => setSeachTerm(event.target.value)} 
+        placeholder="Search for items..." 
+        list="titles" />
+        </p>
       <datalist id="titles">
         {props.products.map(product =>
           <option key={product.id}>{product.name}</option>
         )}
       </datalist>
-      {props.products.map((product) => (
-        <div key={product.id}>
-          <img src={product.images[0] ? product.images[0].url : "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"} class="img-thumbnail" alt="..."></img>
-          <h2>{product.name}</h2>
-          <h2>{product.price}</h2>
-          <h2>{product.supplier_id}</h2>
-          <button onClick={() => props.onShowProduct(product)}>More info</button>
-        </div>
+      <div className="cards">
+        {props.products.filter((product) => product.name.toLowerCase().includes(searchTerm.toLowerCase())).map(product => (
+          <div key={product.id}>
+            <img src={product.images[0] ? product.images[0].url : "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"} class="img-thumbnail" alt="..."></img>
+            <h2>{product.name}</h2>
+            <h2>{product.price}</h2>
+            <h2>{product.supplier_id}</h2>
+            <button onClick={() => props.onShowProduct(product)}>More info</button>
+          </div>
       ))}
+      </div>
     </div>
   );
 }
